508966ead281dd3b7aa09a0a06636381246207fb
Hadi Gharibi <hady.gharibi@gmail.com>
2025-10-25 00:24:15 +0200

    fix ruff

--- src/stripje/__init__.py
+++ src/stripje/__init__.py
@@ -7,11 +7,9 @@ from . import estimators, transformers
 from .__version__ import __version__
 from .fast_pipeline import compile_pipeline, get_supported_transformers
 
-
 __all__ = [
     "__version__",
     # Core compilation
     "compile_pipeline",
     "get_supported_transformers",
-
 ]
