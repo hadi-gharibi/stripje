6f100cc12f9fd0493cea28b5cbf6a4211ca60bd9
Hadi Gharibi <hady.gharibi@gmail.com>
2025-10-25 00:14:56 +0200

    update init for new profiler

--- src/stripje/__init__.py
+++ src/stripje/__init__.py
@@ -6,21 +6,12 @@ Sklean - Make sklearn pipelines lean and efficient with fast single-row inferenc
 from . import estimators, transformers
 from .__version__ import __version__
 from .fast_pipeline import compile_pipeline, get_supported_transformers
-from .profiling import (
-    CompiledPipelineProfiler,
-    PipelineProfiler,
-    ProfileResult,
-    profile_pipeline_compilation,
-)
+
 
 __all__ = [
     "__version__",
     # Core compilation
     "compile_pipeline",
     "get_supported_transformers",
-    # Profiling
-    "PipelineProfiler",
-    "CompiledPipelineProfiler",
-    "ProfileResult",
-    "profile_pipeline_compilation",
+
 ]
